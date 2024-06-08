64d3d3a8eaf6b1bb043a8ad6f4a83352d4164893
xavier.emmanuel.hr <javier.emanuel.hr@gmail.com>
2024-06-08 00:15:28 -0600

    almost done with the webResume project

---
+++ resume/src/sections/header.jsx
@@ -0,0 +1,22 @@
+import reactLogo from '../assets/react.svg'
+import { Button } from "@/components/ui/button"
+
+export default function Header() {
+  return (
+    <div class='fixed flex flex-row p-2 w-full bg-gradient-to-r from-purple-500 to-pink-500 font-montserrat font-semibold'>
+
+        <img src={reactLogo} class=' flex-none App-logo hover:animate-spin' alt='logo' />
+
+        <h1 class='flex-none text-white text-2xl pl-2'> javier.emanuel </h1>
+
+        <nav class='flex-grow text-white hidden sm:block'>  
+            <ul class='flex flex-row justify-end'>
+                <li class='p-2 hover:bg-sky-600'>About</li>
+                <li class='p-2 hover:bg-sky-600'>Projects</li>
+                <li class='p-2 hover:bg-sky-600'>Contact</li>
+            </ul>
+        </nav>
+
+    </div>
+  )
+}
